8845d5f68a1431caf2c96bd3fa2391ab0f0ad0c4
Akash <therealakash13@gmail.com>
2025-11-21 18:37:23 +0530

    socket.io initialized, established connection on both frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,29 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
+const app = express(); // express instance created with name 'app'
+const server = http.createServer(app); // new http server created
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+}); // new socket_io instance created
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello from Chat App...");
 });
 
-app.listen(port, () => `Server is listening on http://localhost:${port}`);
+// socket_io connection event
+io.on("connection", (socket) => {
+  console.log(`A user connected with id: ${socket.id}`);
+});
+
+// start listening on port with http server instance not express instance
+server.listen(port, () =>
+  console.log(`Server is listening on http://localhost:${port}`)
+);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react";
+import { io } from "socket.io-client";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const socketRef = useRef(null); // ensures connection persists across re-renders
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    socketRef.current = io("http://localhost:3000");
+
+    // On connect
+    socketRef.current.on("connect", () => {
+      console.log("Connected with ID:", socketRef.current.id);
+    });
+
+    // On disconnect
+    socketRef.current.on("disconnect", () => {
+      console.log("Disconnected");
+    });
+
+    // Cleanup when component unmounts
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  return <h1>App</h1>;
 }
 
-export default App
+export default App;
